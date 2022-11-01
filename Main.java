b7cedbe3cf741ac6976e1326a27b8ed413498706
Olivia Sarkisian <115608329+sarkisianolivia@users.noreply.github.com>
2022-10-31 22:42:24 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Outlander outlander = new Outlander(36);
+        outlander.steer(35);
+        outlander.accelerate(30);
+    }
+
+
+}
